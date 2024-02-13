95a06e214d742ae3c03b33b24d933de564235ce9
shefali310 <shefpanchal3@gmail.com>
2024-02-13 12:19:59 -0500

    Hero.tsx modify

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <div className=" bg-gray-800 pb-16 pt-3">
       <div className="container mx-auto flex flex-col gap-2">
         <h1 className="text-5xl sm:text-2xl  pl-5  text-white font-bold">
-          Find your next stay
+          Find your next hotel priyashi
         </h1>
         <p className="text-2xl sm:text-xl  pl-5 text-white">
           Search low prices on hotels for your dream vacation...
